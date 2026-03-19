504c633d7ea97b9829ef5e5c491cf8d3a7720fac
aquacat1220 <155734094+aquacat1220@users.noreply.github.com>
2026-03-19 14:50:32 +0000

    Deploying to gh-pages from @ aquacat1220/shatter@30860d20ac49e32aa6b9355a087d2def86bda70c 🚀

---
+++ sw.js
@@ -0,0 +1,8 @@
+// var cacheName = 'shatter-cache';
+
+// /* Serve cached content when offline */
+// self.addEventListener('fetch', function (e) {
+//   e.respondWith(
+//     caches.open(cacheName).then(function (cache) { cache.add(e.request) })
+//   );
+// });
